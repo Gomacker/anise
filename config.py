51635926ebfcb8a21ec8ade74d1ed76e9744c5d9
Gomacker <gomackerrst@gmail.com>
2023-10-08 00:53:17 +0800

    Update

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 
-ROOT_PATH = Path(__file__).parent
+ROOT_PATH = Path('.').parent
 RES_PATH = ROOT_PATH.parent / 'res'
 os.makedirs(RES_PATH, exist_ok=True)
 DATA_PATH = ROOT_PATH.parent / 'data'
